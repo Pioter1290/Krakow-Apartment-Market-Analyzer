878353f0e85eab94f2337558d80f45e35ecee73d
Piotr <piotrkloc3k@gmail.com>
2024-04-14 23:16:42 +0200

    Flask technology

--- app.py
+++ app.py
@@ -1,14 +1,64 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import pandas as pd
 from bs4 import BeautifulSoup
 from urllib import request as urlrequest
 import re
+
+from matplotlib import pyplot as plt
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/generate_chart')
+def generate_chart_bar():
+    # Assuming generate_all_flats() returns flats_info DataFrame
+    flats_info = generate_all_flats()
+
+    dzielnice_count = flats_info['dzielnica'].value_counts()
+
+    plt.figure(figsize=(14, 6))
+    dzielnice_count.plot(kind='bar', color='skyblue', edgecolor='black')
+    plt.title('Liczba ogłoszeń w zależności od dzielnicy')
+    plt.xlabel('Dzielnica')
+    plt.ylabel('Liczba ogłoszeń')
+    plt.xticks(rotation=45)
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.tight_layout()
+
+    img_path = 'static/dzielnice_chart.png'
+    plt.savefig(img_path)
+    plt.close()
+
+    img_url = url_for('static', filename='dzielnice_chart.png')
+    return f'<img src="{img_url}" alt="Bar Chart of Listings by District"/>'
+
+@app.route('/generate_all_flats')
+def generate_all_flats():
+    url = "https://www.otodom.pl/pl/wyniki/sprzedaz/mieszkanie/malopolskie/krakow/krakow/krakow?viewType=listing"
+    pages = []
+    for page_number in range(1, 5):  # Możesz zwiększyć liczbę stron, jeśli potrzebujesz więcej danych
+        req = urlrequest.Request(f'{url}&page={page_number}', headers={'User-Agent': 'Mozilla/5.0'})
+        with urlrequest.urlopen(req) as resp:
+            processed_page = BeautifulSoup(resp.read().decode('utf-8'), "html.parser")
+            pages.append(processed_page)
+
+    flats = []
+
+    for page in pages:
+        for flat_html in page.find_all(class_="css-136g1q2 e88tro00"):
+            flat = {}
+            flat['adres'] = flat_html.find(class_="css-12h460e efr035y1").text.strip()
+            flat_data = str(flat_html.find(class_="css-uki0wd e12r8p6s1"))
+            parse_flat_details(flat, flat_data)
+
+            url_element = flat_html.find('a', attrs={'data-cy': 'listing-item-link'}).get('href')
+            flat['url'] = 'https://www.otodom.pl' + url_element if url_element else 'NONE'
+            flats.append(flat)
+
+    flats_info = pd.DataFrame(flats)
+    return flats_info.to_html() if not flats_info.empty else "Nie znaleziono mieszkań."
 
 @app.route('/process', methods=['GET', 'POST'])
 def process_data_and_plot():
